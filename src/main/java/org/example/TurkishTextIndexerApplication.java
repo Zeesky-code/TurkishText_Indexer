9dbf8463002525d96f03c777485ca55c40152fb2
Zainab Lawal <zainablaw1012@gmail.com>
2024-01-03 18:07:54 +0300

    Switch to spring project

---
+++ src/main/java/org/example/TurkishTextIndexerApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurkishTextIndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurkishTextIndexerApplication.class, args);
+	}
+
+}
